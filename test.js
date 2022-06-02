ea515459bad3f38f40d22e35aaff8205a2f88182
Eriniss <jhs2195@gmail.com>
2022-06-02 10:48:18 +0900

    reviseChapterFrom7to8

--- test.js
+++ test.js
@@ -1,7 +1,9 @@
-const x = 1;
-
-person = {x}
-
-person.x = 4;
-
-console.log(person); // { x: 4 }
+{
+  var count = 0;
+  
+  while (true) {
+    console.log(count);
+    count++;
+    if (count === 3) break; // count가 3일때 break문이 작동한다. 작동시 코드 블록에서 탈출한다.
+  }
+  }
